e5912cef76225a320876cc85c0005d60e4147742
dan <contact@danmyers.net>
2025-03-11 18:26:59 -0500

    Random start and life represented by bits

--- www/index.js
+++ www/index.js
@@ -1,13 +1,14 @@
 import { Universe, Cell } from "wasm-game-of-life";
 import { memory } from "wasm-game-of-life/wasm_game_of_life_bg";
 
-const CELL_SIZE = 5; // px
+const CELL_SIZE = 5;
 const GRID_COLOR = "#CCCCCC";
 const DEAD_COLOR = "#FFFFFF";
 const ALIVE_COLOR = "#000000";
 
-const width = Math.floor((window.innerWidth - 1) / (CELL_SIZE + 1));
-const height = Math.floor((window.innerHeight - 1) / (CELL_SIZE + 1));
+// In order to use width/height we need to up the memory
+const width = 64;//Math.floor((window.innerWidth - 1) / (CELL_SIZE + 1));
+const height = 64;//Math.floor((window.innerHeight - 1) / (CELL_SIZE + 1));
 
 // Construct the universe, and get its width and height.
 const universe = Universe.new(width, height);
@@ -22,9 +23,11 @@ canvas.width = (CELL_SIZE + 1) * width + 1;
 
 const ctx = canvas.getContext('2d');
 
-const renderLoop = () => {
-  universe.tick();
+const sleep = ms => new Promise(r => setTimeout(r, ms));
 
+const renderLoop = async () => {
+  universe.tick();
+  await sleep(75)
   drawGrid();
   drawCells();
 
@@ -35,13 +38,13 @@ const drawGrid = () => {
   ctx.beginPath();
   ctx.strokeStyle = GRID_COLOR;
 
-  // Vertical lines.
+  // Vertical lines
   for (let i = 0; i <= width; i++) {
     ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
     ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
   }
 
-  // Horizontal lines.
+  // Horizontal lines
   for (let j = 0; j <= height; j++) {
     ctx.moveTo(0,                           j * (CELL_SIZE + 1) + 1);
     ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
@@ -53,9 +56,16 @@ const getIndex = (row, column) => {
   return row * width + column;
 };
 
+const bitIsSet = (n, arr) => {
+  const byte = Math.floor(n / 8);
+  const mask = 1 << (n % 8);
+  return (arr[byte] & mask) === mask;
+};
+
 const drawCells = () => {
   const cellsPtr = universe.cells();
-  const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+  const cells = new Uint8Array(memory.buffer, cellsPtr, width * height / 8);
 
   ctx.beginPath();
 
@@ -63,9 +73,9 @@ const drawCells = () => {
     for (let col = 0; col < width; col++) {
       const idx = getIndex(row, col);
 
-      ctx.fillStyle = cells[idx] === Cell.Dead
-        ? DEAD_COLOR
-        : ALIVE_COLOR;
+      ctx.fillStyle = bitIsSet(idx, cells)
+        ? ALIVE_COLOR
+        : DEAD_COLOR;
 
       ctx.fillRect(
         col * (CELL_SIZE + 1) + 1,
